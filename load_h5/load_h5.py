b6555cc32b03ff72a72acdd16eae6df3530f5ead
MikelGarciaData <mikelgarciadata@gmail.com>
2025-10-27 15:58:05 +0100

    changing load file

--- load_h5/load_h5.py
+++ load_h5/load_h5.py
@@ -1,12 +1,18 @@
 import os
+from pathlib import Path
 import h5py
 import scanpy as sc
+import polars as pl
+import numpy as np
+from scipy.sparse import issparse
 
-datapath = "data/adata_Training.h5ad"
+current_path = Path(__file__).parent
+data_folder = (current_path.parent / 'data' / 'adata_Training.h5ad').resolve()
+# data_folder = os.path.abspath(data_folder)
 
-print(f"loading data from {datapath}")
+print(f"loading data from {data_folder}")
 # Cargar el archivo .h5ad
-adata = sc.read_h5ad(datapath)
+adata = sc.read_h5ad(data_folder)
 
 # Ejemplo: mostrar resumen
 print(adata)
@@ -31,3 +37,33 @@ print(type(adata.var))
 # Aligned mapping object
 print(type(adata.obsm)) # UMAP embedding
 # .obsm matrices must length equal to the number of observations as .n_obs
+
+
+# Extraer nombres de genes y células
+print("Extract gene and cell names...")
+
+genes = adata.var_names.to_list()
+cells = adata.obs_names.to_list()
+
+# Obtener la matriz de expresión
+X = adata.X
+
+print("Creating dataframe of cell and genes...")
+
+if issparse(X):
+    X = X.tocoo()  # formato coordenado: más fácil para reconstruir
+    df = pl.DataFrame({
+        "CELL": [cells[i] for i in X.row],
+        "GEN": [genes[j] for j in X.col],
+        "count": X.data
+    })
+else:
+    # Si no es sparse (denso)
+    df = pl.DataFrame({
+        "GEN": np.repeat(genes, len(cells)),
+        "CELL": np.tile(cells, len(genes)),
+        "count": X.T.flatten()  # transpuesta para coincidir con orden (gene, cell)
+    })
+
+print(df.head())
+print(df.shape)
