2717fb11938849796988d23d4232e2ce8718bee1
DhruvaSakhare <dhruvasakhare@gmail.com>
2025-12-07 21:37:25 +0100

    added notebook

--- geneFlow/plot_traj.py
+++ geneFlow/plot_traj.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 # ==============================
 # 1. SETUP
 # ==============================
-SAVE_DIR = "/dtu/blackhole/1a/187738/deep/DL_VCC/geneFlow/results_experiment_200_epoch_128_4"
+SAVE_DIR = "/dtu/blackhole/1a/187738/deep/DL_VCC/geneFlow/results_experiment_200_epoch_256_8"
 time_points = [0.00, 0.25, 0.50, 0.75, 1.00]
 
 # Create figure with 5 subplots
 fig, axes = plt.subplots(1, 5, figsize=(25, 5))
 
-print("Generating 5 side-by-side plots using sc.pl.umap...")
+print("Generating UMAP plots...")
 
 # ==============================
 # 2. LOOP & PLOT
@@ -26,30 +26,22 @@ for i, t in enumerate(time_points):
     if os.path.exists(path):
         print(f"Processing t={t}...")
         
-        # 1. Load Data
+        # Load Data
         adata_t = sc.read_h5ad(path)
         
-        # 2. Compute UMAP (Independent for each timepoint)
+        # Compute UMAP (Independent for each timepoint)
         sc.pp.neighbors(adata_t, use_rep='X', n_neighbors=30)
-        sc.tl.umap(adata_t)
-        
-        # 3. Create 'sample' column if it doesn't exist
-        # (Mapping 'condition' (Gene Name) to 'sample' so your snippet works)
-        if 'sample' not in adata_t.obs:
-            adata_t.obs['sample'] = adata_t.obs['condition']
+        sc.tl.umap(adata_t, random_state=42)
+    
 
-        # 4. Plot using Scanpy
-        # We pass 'ax=ax' to put it in the specific panel
-        # We pass 'show=False' so it doesn't pop up immediately
+        # Plot
         sc.pl.umap(
             adata_t,
-            color="sample",
-            # Setting a smaller point size to prevent overlap
+            color="condition",
             size=2,
             ax=ax,
             show=False, 
             title=f"Time: {t}",
-            # Optional: Turn off legend for first 4 plots to save space
             legend_loc='right margin' if i == 4 else None 
         )
 
